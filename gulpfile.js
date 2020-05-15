760da9d701501f500ef5808115d04441bef20e50
Nastia <my.ananastis@gmail.com>
2020-05-15 09:49:06 +0300

    sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'), // Подключаем Gulp
+sass = require('gulp-sass'); //Подключаем Sass пакет
+watch = require('gulp-watch');
 
-gulp.task('sass', function () {
-  gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+gulp.task('sass', function(){ // Создаем таск "sass"
+return gulp.src('src/sass/*.sass') // Берем источник
+  .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+  .pipe(gulp.dest('src/css')) // Выгружаем результата в папку app/css
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-});
+gulp.task('watch', function(){
+  gulp.watch('src/sass/*.sass', ['sass']);
+  // Other watchers
+})
