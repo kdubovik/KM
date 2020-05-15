956638460b3e4a8467a404065eb51526322fc1fd
Nastia <my.ananastis@gmail.com>
2020-05-15 15:28:58 +0300

    Galery. Button.

--- src/js/script.js
+++ src/js/script.js
@@ -17,13 +17,13 @@ function backToTop() {
   }
 
 
-  var goTopBtn = document.querySelector('.btn-scroll-up');
+  var goTopBtn = document.querySelector('#btn-scroll-up');
 
   window.addEventListener('scroll', trackScroll);
   goTopBtn.addEventListener('click', backToTop);
 
 
-  const anchors = document.querySelectorAll('.btn-scroll-up')
+  const anchors = document.querySelectorAll('#btn-scroll-up')
 
 for (let anchor of anchors) {
   anchor.addEventListener('click', function (e) {
