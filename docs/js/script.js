c5ac006a9069c6492f97ab25742520c6a8b32134
kdubovik <64782470+kdubovik@users.noreply.github.com>
2020-05-22 16:05:55 +0300

    Add files via upload

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 (function() {
   'use strict';
@@ -61,67 +60,3 @@ window.onclick = function(event) {
     }
   }
 }
-=======
-
-(function() {
-  'use strict';
-
-
-function trackScroll(){
-  var scrolled = window.pageYOffset;
-  var coords = document.documentElement.clientHeight;
-
-}
-
-function backToTop() {
-    if (window.pageYOffset > 0) {
-      window.scrollBy(0, -80);
-      setTimeout(backToTop, 20);
-    }
-  }
-
-
-  var goTopBtn = document.querySelector('#btn-scroll-up');
-
-  window.addEventListener('scroll', trackScroll);
-  goTopBtn.addEventListener('click', backToTop);
-
-  var goTopBtn = document.querySelector('#logo-up');
-
-  window.addEventListener('scroll', trackScroll);
-  goTopBtn.addEventListener('click', backToTop);
-
-
-  const anchors = document.querySelectorAll('#btn-scroll-up')
-
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-
-    const blockID = anchor.getAttribute('href')
-
-    document.querySelector(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
-}
-
-})();
-
-function myFunction(){
-  document.getElementById("myMenu").classList.toggle("show");
-}
-window.onclick = function(event) {
-  if (!event.target.matches('.menu__button')) {
-    var dropdowns = document.getElementsByClassName("menu__content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
->>>>>>> e604eb7e954979594c79a2fd0cdc0eb15fe92cbe
--- docs/slick-slider/custom.js
+++ docs/slick-slider/custom.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 $(document).ready(function(){
 
   $('.slick-container').slick();
 
 });
-=======
-$(document).ready(function(){
-
-  $('.slick-container').slick();
-
-});
->>>>>>> e604eb7e954979594c79a2fd0cdc0eb15fe92cbe
